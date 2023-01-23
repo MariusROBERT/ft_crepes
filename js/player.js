48604932bc0440b5ff5d71d5b3519c0fd31407d0
Marius Robert <marobert@z4r10p1.42lyon.fr>
2023-01-23 18:38:14 +0100

    add now playing

--- js/player.js
+++ js/player.js
@@ -1,8 +1,9 @@
 
-function change_music(new_file) {
+function change_music(new_file, new_title) {
     let player = document.querySelector("#player > audio");
 
     player.src = "music/" + new_file;
     player.querySelector("a").href = "music/" + new_file;
     player.play();
+    document.querySelector("#player h3:nth-child(2)").innerHTML = new_title;
 }
